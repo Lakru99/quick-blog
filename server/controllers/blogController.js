d55e1348f71a709502d9971ed96c23da0657f9bb
lakru999 <lakru999@gmail.com>
2025-07-27 01:55:19 +0530

    blog added api created

---
+++ server/controllers/blogController.js
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import Blog from "../models/Blog.js";
+import imagekit from "../config/imageKit.js";
+
+export const addBlog = async (req, res) => {
+  try {
+    const { title, subTitle, description, category, isPublished } = JSON.parse(req.body.blog);
+
+    const imageFile = req.file;
+
+    if (!title || !description || !category || !imageFile) {
+      return res.json({ success: false, message: "Missing required fields" });
+    }
+
+    const fileBuffer = fs.readFileSync(imageFile.path);
+
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/blogs"
+    });
+    const optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { quality: "auto" },
+        { format: "webp" },
+        { width: "1280" },
+      ],
+    });
+    
+    const image = optimizedImageUrl;
+
+    await Blog.create({ title, subTitle, description, category, image, isPublished });
+
+    res.json({
+      success: true,
+      message: "Blog Added Successfully",
+    });
+  } catch (error) {
+    console.error("Caught error:", error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
